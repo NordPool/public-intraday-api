dacc2b96aaef36506e4bcaf6bf44ea0dc28edb7c
Alex Bykov <aleksandr.bykov@oxagile.com>
2017-10-02 16:43:10 +0300

    DEV-1805 : Generate java DTOs from JSON/XML schema files
     - Removed generics from trade models

--- src/main/java/com/nordpool/id/publicapi/v1/PrivateTradeRow.java
+++ src/main/java/com/nordpool/id/publicapi/v1/PrivateTradeRow.java
@@ -9,14 +9,24 @@ package com.nordpool.id.publicapi.v1;
 import com.nordpool.id.publicapi.v1.base.BaseTradeRow;
 import com.nordpool.id.publicapi.v1.trade.leg.PrivateTradeLeg;
 
+import java.util.List;
 import java.util.Objects;
 
-public class PrivateTradeRow extends BaseTradeRow<PrivateTradeLeg> {
+public class PrivateTradeRow extends BaseTradeRow {
+    private List<PrivateTradeLeg> legs;
     private Long cancellationFee;
     private String cancellationDeadLine;
     private Long revisionNo;
 
     public PrivateTradeRow() {
+    }
+
+    public List<PrivateTradeLeg> getLegs() {
+        return legs;
+    }
+
+    public void setLegs(List<PrivateTradeLeg> legs) {
+        this.legs = legs;
     }
 
     public Long getCancellationFee() {
@@ -49,20 +59,22 @@ public class PrivateTradeRow extends BaseTradeRow<PrivateTradeLeg> {
         if (!(o instanceof PrivateTradeRow)) return false;
         if (!super.equals(o)) return false;
         PrivateTradeRow that = (PrivateTradeRow) o;
-        return Objects.equals(cancellationFee, that.cancellationFee) &&
+        return Objects.equals(legs, that.legs) &&
+                Objects.equals(cancellationFee, that.cancellationFee) &&
                 Objects.equals(cancellationDeadLine, that.cancellationDeadLine) &&
                 Objects.equals(revisionNo, that.revisionNo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), cancellationFee, cancellationDeadLine, revisionNo);
+        return Objects.hash(super.hashCode(), legs, cancellationFee, cancellationDeadLine, revisionNo);
     }
 
     @Override
     public String toString() {
         return "PrivateTradeRow{" +
-                "cancellationFee=" + cancellationFee +
+                "legs=" + legs +
+                ", cancellationFee=" + cancellationFee +
                 ", cancellationDeadLine='" + cancellationDeadLine + '\'' +
                 ", revisionNo=" + revisionNo +
                 "} " + super.toString();
--- src/main/java/com/nordpool/id/publicapi/v1/PublicTradeRow.java
+++ src/main/java/com/nordpool/id/publicapi/v1/PublicTradeRow.java
@@ -9,14 +9,41 @@ package com.nordpool.id.publicapi.v1;
 import com.nordpool.id.publicapi.v1.base.BaseTradeRow;
 import com.nordpool.id.publicapi.v1.trade.leg.PublicTradeLeg;
 
-public class PublicTradeRow extends BaseTradeRow<PublicTradeLeg> {
+import java.util.List;
+import java.util.Objects;
+
+public class PublicTradeRow extends BaseTradeRow {
+    private List<PublicTradeLeg> legs;
 
     public PublicTradeRow() {
+    }
+
+    public List<PublicTradeLeg> getLegs() {
+        return legs;
+    }
+
+    public void setLegs(List<PublicTradeLeg> legs) {
+        this.legs = legs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PublicTradeRow)) return false;
+        if (!super.equals(o)) return false;
+        PublicTradeRow that = (PublicTradeRow) o;
+        return Objects.equals(legs, that.legs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), legs);
     }
 
     @Override
     public String toString() {
         return "PublicTradeRow{" +
+                "legs=" + legs +
                 "} " + super.toString();
     }
 }
